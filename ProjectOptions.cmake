852e61f2fac7d6bbe4395f3905f295b7635f0706
Qi Gao <qi.gao2012@gmail.com>
2026-09-20 19:48:24 +0800

    build: make TurboScript workflow support optional

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -4,6 +4,7 @@ set(CMAKE_COLOR_DIAGNOSTICS ON)
 
 # building the tests
 option(ENABLE_TESTS "Enable the tests" OFF)
+option(PRAKTOR_ENABLE_SCRIPT "Enable TurboScript workflow script support" ON)
 
 set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 
