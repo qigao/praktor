6edb212d5cf79b1eefcc4d83271f2df299d24660
Qi Gao <qi.gao2012@gmail.com>
2026-09-20 19:22:59 +0800

    build: add optional script engine mode

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -4,6 +4,7 @@ set(CMAKE_COLOR_DIAGNOSTICS ON)
 
 # building the tests
 option(ENABLE_TESTS "Enable the tests" OFF)
+option(ENABLE_SCRIPT_ENGINE "Enable the TurboScript-backed workflow script engine" ON)
 
 set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 
